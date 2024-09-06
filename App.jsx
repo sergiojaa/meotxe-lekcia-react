90cc25630205585e48ecd5d7d5855a3db49d7cab
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-09-06 10:58:47 +0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-light-green">
+        hello world
+        </h1>
+      <p>sdsd sdsds sd  </p>
+    </>
+  )
+}
+
+export default App
